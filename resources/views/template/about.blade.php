7e9ec8ff6e4cf7a0abb777368763c506193c16cf
Zubs <zubairidrisaweda@gmail.com>
2020-06-05 18:49:35 +0100

    Started work

---
+++ resources/views/template/about.blade.php
@@ -0,0 +1,353 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>About Us</title>
+    <link rel="icon" href="{{ asset('template/img/favicon.png') }}">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('template/css/bootstrap.min.css') }}">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="{{ asset('template/css/animate.css') }}">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="{{ asset('template/css/owl.carousel.min.css') }}">
+    <!-- themify CSS -->
+    <link rel="stylesheet" href="{{ asset('template/css/themify-icons.css') }}">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="{{ asset('template/css/all.css') }}">
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="{{ asset('template/css/flaticon.css') }}">
+    <!-- magnific popup CSS -->
+    <link rel="stylesheet" href="{{ asset('template/css/magnific-popup.css') }}">
+    <!-- nice select CSS -->
+    <link rel="stylesheet" href="{{ asset('template/css/nice-select.css') }}">
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="{{ asset('template/css/slick.css') }}">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="{{ asset('template/css/style.css') }}">
+</head>
+
+<body>
+    <!--::header part start::-->
+    <header class="main_menu home_menu">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <a class="navbar-brand" href="/">
+        <img src="logo.png" width="70" height="30" margin-left="3%" alt="" loading="lazy">
+        </a>
+        <button class="navbar-toggler" style="color: white;" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+                        <div class="collapse navbar-collapse main-menu-item justify-content-end"
+                            id="navbarSupportedContent">
+                            <ul class="navbar-nav align-items-center">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/about">About Us</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/contact">Contact Us</a>
+                                </li>
+                                <!-- This Should Contain a PDF link About Efico  -->
+                                <li class="nav-item">
+                                    <a class="nav-link" href="faqs.html">Learn More</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header part end-->
+
+    <!-- breadcrumb start-->
+    <section class="breadcrumb breadcrumb_bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb_iner">
+                        <div class="breadcrumb_iner_item text-center">
+                            <h2>about us</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- breadcrumb start-->
+    <!-- about part end-->
+    <section class="about_us padding_top">
+        <div class="container">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-lg-6">
+                    <div class="about_us_img">
+                        <img src="{{ asset('template/img/top_service.png') }}" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="about_us_text">
+                        <h5>
+                            2019<br><span>since</span>
+                        </h5>
+                        <h2>Our Mission</h2>                            
+                        <p>We use our personalized self-assement test to help undergraduates know 
+                            themselves and channel skills in the Direction of their Natural talent. 
+                        </p>
+                        <h2>Our Vision</h2>                            
+                        <p>To churn out exceptional graduates every year, and 
+                            instilling Efico in the minds on every undergraduate.
+                        </p>
+                        <h2>Our Purpose</h2>                            
+                        <p>We connect the significances of the academia in relation to the industry, 
+                            thereby making undergraduates truly successful upon graduation.
+                        </p>
+                        <div class="banner_item">
+                            <div class="single_item">
+                                <h2> <span class="count">500</span>+</h2>
+                                <h5>We have over 500 Student Trained in Nigeria Universities</h5>
+                            </div>
+                            <div class="single_item">
+                                <h2><span class="count">40</span>+</h2>
+                                <h5>We have over 40 Student Ambassadors from Various Universities Across Nigeria</h5>
+                            </div>
+                            <div class="single_item">
+                                <h2><span class="count">30</span>+</h2>
+                                <h5>We have Reached out to over 30 Private and Public Universities in Nigeria.</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <div class="col-lg-12">
+                    <div class="text-center about_btn">
+                        <h2>Our Core Values</h2> <br>
+                        <p class="btn_3 ">E-efficient</p> <p> We are efficient, we always maximize your potential for that personalized skill </p>
+                        <p class="btn_3 ">F- Faithful</p> <p>We are faithful, we adhere firmly to the course of making you exceptional after graduation.</p>
+                        <p class="btn_3 ">I-integrity</p> <p> We stand as an entity of integrity, we are true to our word.</p>
+                        <p class="btn_3 ">C- communication</p><p> We love you to communicate, because that way, we can serve you better. </p>
+                        <p class="btn_3 ">O-organization</p> <p>We love you to communicate, because that way, we can serve you better.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- about part end-->
+
+    <!--::passion part start::-->
+    <section class="passion_part passion_section_padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-5 col-md-8">
+                    <div class="section_tittle">
+                        <h2>Our Team</h2>
+                        <p>Meet Efico Directors and Team Members </p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6 col-lg-4 col-xl-4">
+                    <div class="single-home-passion">
+                        <div class="card">
+                            <img src="{{ asset('template/img/passion/passion_1.png') }}" class="card-img-top" alt="blog">
+                            <div class="card-body">
+                                <a href="#">
+                                    <h5 class="card-title">Directors</h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                 <div class="col-sm-6 col-lg-4 col-xl-4">
+                    <div class="single-home-passion">
+                        <div class="card">
+                            <img src="{{ asset('template/img/passion/passion_3.png') }}" class="card-img-top" alt="blog">
+                            <div class="card-body">
+                                <a href="#">
+                                    <h5 class="card-title"> Team Members </h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div> 
+                <div class="col-sm-6 col-lg-4 col-xl-4">
+                    <div class="single-home-passion">
+                        <div class="card">
+                            <img src="{{ asset('template/img/passion/passion_3.png') }}" class="card-img-top" alt="blog">
+                            <div class="card-body">
+                                <a href="#">
+                                    <h5 class="card-title">Team Members</h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--::passion part end::-->
+
+     <!--::our client part start::-->
+     <section class="client_part padding_bottom">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-6">
+                    <div class="section_tittle text-center">
+                        <!-- I dont know what to put there you can Change the title -->
+                        <h2>EFICO Universities Based</h2>  
+                    </div>
+                </div>
+            </div>
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <div class="client_logo owl-carousel">
+                        <div class="single_client_logo">
+                            <img src="{{ asset('template/img/client_logo/Logo_1.png') }}" alt="">
+                        </div>
+                        <div class="single_client_logo">
+                            <img src="{{ asset('template/img/client_logo/Logo_2.png') }}" alt="">
+                        </div>
+                        <div class="single_client_logo">
+                            <img src="{{ asset('template/img/client_logo/Logo_3.png') }}" alt="">
+                        </div>
+                        <div class="single_client_logo">
+                            <img src="{{ asset('template/img/client_logo/Logo_4.png') }}" alt="">
+                        </div>
+                        <div class="single_client_logo">
+                            <img src="{{ asset('template/img/client_logo/Logo_5.png') }}" alt="">
+                        </div> 
+                        <div class="single_client_logo">
+                            <img src="{{ asset('template/img/client_logo/Logo_6.jpg') }}" alt="">
+                        </div>
+                        <div class="single_client_logo">
+                            <img src="{{ asset('template/img/client_logo/Logo_7.jpg') }}" alt="">
+                        </div>
+                        <div class="single_client_logo">
+                            <img src="{{ asset('template/img/client_logo/Logo_8.jpg') }}" alt="">
+                        </div>
+                        <div class="single_client_logo">
+                            <img src="{{ asset('template/img/client_logo/Logo_9.jpg') }}" alt="">
+                        </div>
+                        <div class="single_client_logo">
+                            <img src="{{ asset('template/img/client_logo/Logo_10.jpg') }}" alt="">
+                        </div>
+                        <div class="single_client_logo">
+                            <img src="{{ asset('template/img/client_logo/Logo_11.jpg') }}" alt="">
+                        </div>
+                        <div class="single_client_logo">
+                            <img src="{{ asset('template/img/client_logo/Logo_12.png') }}" alt="">
+                        </div>
+                        <div class="single_client_logo">
+                            <img src="{{ asset('template/img/client_logo/Logo_13.jpg') }}" alt="">
+                        </div>
+                        <div class="single_client_logo">
+                            <img src="{{ asset('template/img/client_logo/Logo_14.jpg') }}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--::our client part end::-->
+
+    <!--::footer_part start::-->
+    <footer class="footer_part">
+        <div class="container">
+            <div class="row justify-content-around">
+                <div class="col-sm-6 col-lg-2">
+                    <div class="single_footer_part">
+                        <h4>Quick link</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/about">About Us</a></li>
+                            <li><a href="/contact">Contact Us</a></li>
+                            <li><a href="#">Learn More</a></li>
+                            <li><a href="#">Our Programs</a></li>
+                        </ul>
+                    </div>
+                </div> 
+                <!-- Subcribe for new letter start-->
+                <div class="col-sm-6 col-lg-3">
+                    <div class="single_footer_part">
+                        <h4>Newsletter</h4>
+                        <p>Subscribe to Our Newsletter to keep you Updated</p>
+                        <div id="mc_embed_signup">
+                            <form target="_blank"
+                                action="#"
+                                method="get" class="subscribe_form relative mail_part" required>
+                                <input type="email" name="email" id="newsletter-form-email" placeholder="Email Address"
+                                    class="placeholder hide-on-focus" onfocus="this.placeholder = ''"
+                                    onblur="this.placeholder = ' Email Address '" required="" type="email">
+                                <button type="submit" name="submit" id="newsletter-submit"
+                                    class="email_icon newsletter-submit button-contactForm"><i
+                                        class="far fa-paper-plane"></i></button>
+                                <div class="mt-10 info"></div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="copyright_text">
+                        <p>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved </p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="footer_icon social_icon">
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="single_social_icon"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="#" class="single_social_icon"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#" class="single_social_icon"><i class="fab fa-youtube"></i></a></li>
+                            <li><a href="#" class="single_social_icon"><i class="fab fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--::footer_part end::-->
+
+    <!-- jquery plugins here-->
+
+    <script src="{{ asset('template/js/jquery-1.12.1.min.js') }}"></script>
+    <!-- popper js -->
+    <script src="{{ asset('template/js/popper.min.js') }}"></script>
+    <!-- bootstrap js -->
+    <script src="{{ asset('template/js/bootstrap.min.js') }}"></script>
+    <!-- easing js -->
+    <script src="{{ asset('template/js/jquery.magnific-popup.js') }}"></script>
+    <!-- swiper js -->
+    <script src="{{ asset('template/js/swiper.min.js') }}"></script>
+    <script src="{{ asset('template/js/wow.min.js') }}"></script>
+    <script src="{{ asset('template/js/jquery.smooth-scroll.min.js') }}"></script>
+    <!-- swiper js -->
+    <script src="{{ asset('template/js/masonry.pkgd.js') }}"></script>
+    <!-- particles js -->
+    <script src="{{ asset('template/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('template/js/jquery.nice-select.min.js') }}"></script>
+    <!-- swiper js -->
+    <script src="{{ asset('template/js/slick.min.js') }}"></script>
+    <script src="{{ asset('template/js/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('template/js/waypoints.min.js') }}"></script>
+    <script src="{{ asset('template/js/countdown.jquery.min.js') }}"></script>
+    <!-- contact js -->
+    <script src="{{ asset('template/js/jquery.ajaxchimp.min.js') }}"></script>
+    <script src="{{ asset('template/js/jquery.form.js') }}"></script>
+    <script src="{{ asset('template/js/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('template/js/mail-script.js') }}"></script>
+    <script src="{{ asset('template/js/contact.js') }}"></script>
+    <!-- custom js -->
+    <script src="{{ asset('template/js/custom.js') }}"></script>
+</body>
+
+</html>
